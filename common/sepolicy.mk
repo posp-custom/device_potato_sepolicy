Cosmic-OS/device_cosmic_sepolicy

sepolicy: common: add rules for AOSiP vibrator hal
Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773

common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/aosip/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/aosip/sepolicy/common/vendor

posp-custom/device_potato_sepolicy

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/potato/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/potato/sepolicy/common/vendor
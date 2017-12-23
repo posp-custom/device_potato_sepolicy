96e897ae8c344fec1f81938e51b64b728b519a0e
Demon000 <demonsingur@gmail.com>
2017-12-23 15:07:01 +0200

    sepolicy: common: add rules for AOSiP vibrator hal
    Change-Id: I1e8aba0e0ce1a51cfcd54c734d58f10f0613f773
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -8,3 +8,6 @@ BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/potato/sepolicy/common/private
+
+BOARD_SEPOLICY_DIRS += \
+    device/potato/sepolicy/common/vendor
