Cosmic-OS/device_cosmic_sepolicy

sepolicy: Move AOSiP policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from AOSiP
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/aosip/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/aosip/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/aosip/sepolicy/common/private

posp-custom/device_potato_sepolicy

Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from Potato
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/potato/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/potato/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/potato/sepolicy/common/private