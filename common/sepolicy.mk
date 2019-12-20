44fda5d0397a094ce390d0e0ade0cecb126428c7
rajadeja <raj.jdja@gmail.com>
2019-12-20 16:58:29 +0000

    It's potato
    Change-Id: I80c13f0f2ffb35d511e914569046b08e72c371d9

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,6 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from potato
 #
 
 ifeq ($(TARGET_COPY_OUT_VENDOR), vendor)
@@ -10,17 +10,17 @@ endif
 endif
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/potato/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/potato/sepolicy/common/private
 
 ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/system
+    device/potato/sepolicy/common/dynamic \
+    device/potato/sepolicy/common/system
 else
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/dynamic \
-    device/lineage/sepolicy/common/vendor
+    device/potato/sepolicy/common/dynamic \
+    device/potato/sepolicy/common/vendor
 endif
